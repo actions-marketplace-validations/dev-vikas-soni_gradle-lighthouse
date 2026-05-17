a7953787fbe67565b7f98ad326010656dc1ce0ca
vikassoni <vikasacsoni9211@gmail.com>
2026-05-17 19:52:01 +0530

    release: 2.2.0
    - Bumped project version from 2.1.1 to 2.2.0 in `gradle.properties`.
    - Updated the lighthouse plugin version to 2.2.0 in the example `build.gradle.kts`.

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java-library")
-    id("io.github.dev-vikas-soni.lighthouse") version "2.1.1"
+    id("io.github.dev-vikas-soni.lighthouse") version "2.2.0"
 }
 
 // 1. DANGEROUS REPO: Should trigger JCenter Sunset Error
